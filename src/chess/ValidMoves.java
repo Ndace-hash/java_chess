6e30c8d6fe512dfa0a960dffafce3833fa113f26
Zaccheaus(Ndace-hash) <zaccheausyisa00@gmail.com>
2023-12-09 23:00:04 +0100

    added double steps for pawns on their starting squares

--- src/chess/ValidMoves.java
+++ src/chess/ValidMoves.java
@@ -57,7 +57,7 @@ public class ValidMoves {
 		for (int directionIndex = startDirectionIndex; directionIndex < endDirectionIndex; directionIndex++) {
 			for (int n = 0; n < NumberOfSquaresToEdge[startSquare][directionIndex]; n++) {
 //				out of bound index
-				int targetSquare = startSquare + directionOffsets[directionIndex] * (n +1) ;
+				int targetSquare = startSquare + directionOffsets[directionIndex] * (n + 1);
 
 				if (targetSquare >= 0 && targetSquare < 64) {
 					int pieceOnTargetSquare = Board.squares[targetSquare];
@@ -83,20 +83,30 @@ public class ValidMoves {
 		int startDirIndex = (Pieces.isColour(piece, Pieces.black)) ? 1 : 0;
 		int endDirIndex = (Pieces.isColour(piece, Pieces.white)) ? 1 : 2;
 		for (int directionIndex = startDirIndex; directionIndex < endDirIndex; directionIndex++) {
-			for (int n = 1; n <= NumberOfSquaresToEdge[startSquare][directionIndex]; n++) {
-				int targetSquare = startSquare + directionOffsets[directionIndex] * n;
 
-				if (targetSquare >= 0 && targetSquare < 64) {
-					int pieceOnTargetSquare = Board.squares[targetSquare];
+			int targetSquare = startSquare + directionOffsets[directionIndex];
+			int rank = startSquare / 8;
+			if (targetSquare >= 0 && targetSquare < 64) {
+				int pieceOnTargetSquare = Board.squares[targetSquare];
 
+				if (Pieces.isColour(pieceOnTargetSquare, friendlyPiece))
+					continue;
+				if (Pieces.isColour(pieceOnTargetSquare, opponentPiece))
+					continue;
+
+				moves.add(new Move(startSquare, targetSquare));
+
+				if (rank == 6 || rank == 2) {
+					targetSquare = startSquare + directionOffsets[directionIndex] * 2;
 					if (Pieces.isColour(pieceOnTargetSquare, friendlyPiece))
-						break;
+						continue;
+					if (Pieces.isColour(pieceOnTargetSquare, opponentPiece))
+						continue;
 
 					moves.add(new Move(startSquare, targetSquare));
 
-					if (Pieces.isColour(pieceOnTargetSquare, opponentPiece))
-						break;
 				}
+
 			}
 		}
 	}
